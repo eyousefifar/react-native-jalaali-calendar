dd1ee4210ee709bbf139ebb980474ff05295b2ba
erfan <eyousefifar@gmail.com>
2018-07-07 23:41:48 +0430

    jalaali days list

--- JPicker/jDay.js
+++ JPicker/jDay.js
@@ -1,31 +1,39 @@
 import React from "react";
-import { TouchableOpacity, StyleSheet, FlatList, Text } from "react-native";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Text,
+  View
+} from "react-native";
 import range from "lodash/range";
 import Day from "../Jcalendar/Day";
 export default class JDay extends React.Component {
   _keyExtractor = (item, index) => item + index;
-  _getItemLayout = (data, index) => {
-    return { length: 15, offset: 15 * index, index: index };
-  };
+  // _getItemLayout = (data, index) => {
+  //   return { length: 15, offset: 15 * index, index: index };
+  // };
   _renderJDay = ({ item, index }) => {
     return (
-      <Day
-        index={index}
-        currentDay={this.props.currentMonth}
-        setDay={this.props.setMonth}
-        thisDay={item}
-      />
+      <View style={{ width: 30, height: "100%" }}>
+        <Day
+          index={index}
+          currentDay={this.props.currentDay}
+          setDay={this.props.setDay}
+          thisDay={item}
+        />
+      </View>
     );
   };
   render() {
-    const jData = range(1, this.props.jDayInMonth, 1); //TODO: this can be optimized more
+    const jData = range(1, this.props.jDayInMonth + 1, 1); //TODO: this can be optimized more
     return (
       <FlatList
         data={jData}
         style={{ flex: 1 }}
         horizontal
         inverted
-        getItemLayout={this._getItemLayout}
+        //getItemLayout={this._getItemLayout}
         keyExtractor={this._keyExtractor}
         renderItem={this._renderJDay}
       />
