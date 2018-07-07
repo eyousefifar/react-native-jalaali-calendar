74903b211dde87cef12cbd5b300db53257bfc08f
erfan <eyousefifar@gmail.com>
2018-07-07 18:58:41 +0430

    seperate component for JMonth

---
+++ Jcalendar/JMonth/JMonthItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+export default props => (
+  <TouchableOpacity
+    onPress={() => {
+      props.setMonth(props.index + 1);
+    }}
+    style={styles.container}
+  >
+    <Text
+      style={[
+        styles.jMonthText,
+        { color: props.currentMonth == index + 1 ? "#0029ff" : "black" }
+      ]}
+    >
+      {props.monthName}
+    </Text>
+  </TouchableOpacity>
+);
+const styles = StyleSheet.create({
+  container: {
+    width: 60,
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  jMonthText: {
+    fontSize: 13,
+    fontWeight: "bold",
+    textAlign: "center",
+    textAlignVertical: "center"
+  }
+});
