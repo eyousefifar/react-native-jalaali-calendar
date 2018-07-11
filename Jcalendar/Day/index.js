505fd0b6ec520362648808dc7b924ba87e0a803f
erfan yousefifar <eyousefifar@gmail.com>
2018-07-12 00:23:42 +0430

    changed to pureComponents and added some changes

--- Jcalendar/Day/index.js
+++ Jcalendar/Day/index.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
 import { persianNumber } from "../utils";
-export default class Day extends React.Component {
+export default class Day extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.selected = undefined;
+  }
+  // shouldComponentUpdate(nextProps) {
+  //   if (this.props.thisDay != nextProps.currentDay && !this.selected) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+  _setDay = () => {
+    let { thisDay } = this.props;
+    if (thisDay != null) {
+      this.props.setDay(thisDay);
+    }
+  };
   render() {
-    const { thisDay, currentDay } = this.props;
-    const selected = thisDay == currentDay ? "blue" : "black";
+    this.selected = this.props.thisDay == this.props.currentDay;
     return (
-      <TouchableOpacity
-        style={styles.container}
-        onPress={() => {
-          if (thisDay != null) {
-            this.props.setDay(thisDay);
-          }
-        }}
-      >
-        <Text style={{ color: selected }}>{persianNumber(thisDay)}</Text>
+      <TouchableOpacity style={styles.container} onPress={this._setDay}>
+        <Text style={this.selected ? styles.daySelected : styles.day}>
+          {persianNumber(this.props.thisDay)}
+        </Text>
       </TouchableOpacity>
     );
   }
@@ -23,6 +33,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    margin: StyleSheet.hairlineWidth
+  },
+  day: {
+    fontSize: 18,
+    color: "black"
+  },
+  daySelected: {
+    fontSize: 20,
+    color: "blue"
   }
 });
