456098b311679f97d001212dab88d0db0031675b
erfan yousefifar <eyousefifar@gmail.com>
2018-07-12 00:25:18 +0430

    some changes for initial index and performance

--- Jcalendar/JMonth/index.js
+++ Jcalendar/JMonth/index.js
@@ -2,8 +2,15 @@ import React from "react";
 import { TouchableOpacity, StyleSheet, FlatList, Text } from "react-native";
 import { jMonths } from "../utils";
 import JMonthItem from "./JMonthItem";
-export default class JMonth extends React.Component {
-  _keyExtractor = (item, index) => item + index;
+export default class JMonth extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.selectedMonthIndex = props.currentMonth - 1;
+  }
+  _getItemLayout = (data, index) => {
+    return { length: 60, offset: 60 * index, index: index };
+  };
+  _keyExtractor = (item, index) => `${item} + ${item}`;
   _renderJMonth = ({ item, index }) => {
     return (
       <JMonthItem
@@ -21,6 +28,8 @@ export default class JMonth extends React.Component {
         style={{ flex: 1 }}
         horizontal
         inverted
+        initialScrollIndex={this.selectedMonthIndex}
+        getItemLayout={this._getItemLayout}
         keyExtractor={this._keyExtractor}
         renderItem={this._renderJMonth}
       />
