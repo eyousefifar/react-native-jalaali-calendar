b5d79eec6710d8d84b03b22579538c7d0b0e8c88
erfan yousefifar <eyousefifar@gmail.com>
2018-07-12 00:26:03 +0430

    added get item layout method and styles and changed to pure component

--- Jcalendar/JYear/index.js
+++ Jcalendar/JYear/index.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { TouchableOpacity, StyleSheet, FlatList, Text } from "react-native";
+import { View, StyleSheet, FlatList, Text } from "react-native";
 import range from "lodash/range";
 import JYearItem from "./JYearItem";
-export default class JYear extends React.Component {
+export default class JYear extends React.PureComponent {
   constructor(props) {
     super(props);
     this.jYearRange = range(props.startYear, props.endYear + 1, 1);
+    this.selectedYearIndex = props.currentYear - props.startYear;
   }
-  _keyExtractor = (item, index) => item + index;
+  _getItemLayout = (data, index) => {
+    return { length: 60, offset: 60 * index, index: index };
+  };
+  _keyExtractor = (item, index) => `${item} + ${index}`;
   _renderJYear = ({ item, index }) => {
     return (
       <JYearItem
@@ -20,14 +24,25 @@ export default class JYear extends React.Component {
   };
   render() {
     return (
-      <FlatList
-        data={this.jYearRange}
-        style={{ flex: 1 }}
-        horizontal
-        inverted
-        keyExtractor={this._keyExtractor}
-        renderItem={this._renderJYear}
-      />
+      <View style={style.container} pointerEvents={"box-none"}>
+        <FlatList
+          data={this.jYearRange}
+          style={{ flex: 1 }}
+          horizontal
+          inverted
+          initialScrollIndex={this.selectedYearIndex}
+          getItemLayout={this._getItemLayout}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderJYear}
+        />
+      </View>
     );
   }
 }
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "#d3d3d3"
+  }
+});
