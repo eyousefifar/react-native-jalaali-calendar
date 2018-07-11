9aadc84e378ca9598f814cacc0a8b60590aceb26
erfan yousefifar <eyousefifar@gmail.com>
2018-07-12 00:22:07 +0430

    more optimized Jday

--- JPicker/jDay.js
+++ JPicker/jDay.js
@@ -8,35 +8,54 @@ import {
 } from "react-native";
 import range from "lodash/range";
 import Day from "../Jcalendar/Day";
-export default class JDay extends React.Component {
-  _keyExtractor = (item, index) => item + index;
-  // _getItemLayout = (data, index) => {
-  //   return { length: 15, offset: 15 * index, index: index };
-  // };
+const JDayItem = props => {
+  return (
+    <View style={style.container}>
+      <Day
+        index={props.index}
+        currentDay={props.currentDay}
+        setDay={props.setDay}
+        thisDay={props.thisDay}
+      />
+    </View>
+  );
+};
+export default class JDay extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.selectedDayIndex = this.props.currentDay - 1;
+  }
+  _keyExtractor = (item, index) => `${item} + ${index}`;
+  _getItemLayout = (data, index) => {
+    return { length: 30, offset: 30 * index, index: index };
+  };
   _renderJDay = ({ item, index }) => {
     return (
-      <View style={{ width: 30, height: "100%" }}>
-        <Day
-          index={index}
-          currentDay={this.props.currentDay}
-          setDay={this.props.setDay}
-          thisDay={item}
-        />
-      </View>
+      <JDayItem
+        index={index}
+        currentDay={this.props.currentDay}
+        setDay={this.props.setDay}
+        thisDay={item}
+      />
     );
   };
   render() {
     const jData = range(1, this.props.jDayInMonth + 1, 1); //TODO: this can be optimized more
+
     return (
       <FlatList
         data={jData}
         style={{ flex: 1 }}
         horizontal
         inverted
-        //getItemLayout={this._getItemLayout}
+        initialScrollIndex={this.selectedDayIndex}
+        getItemLayout={this._getItemLayout}
         keyExtractor={this._keyExtractor}
         renderItem={this._renderJDay}
       />
     );
   }
 }
+const style = StyleSheet.create({
+  container: { width: 30, height: "100%" }
+});
