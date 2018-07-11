e2167fb0fa0b93bd7773b94446a877a75b9ef399
erfan yousefifar <eyousefifar@gmail.com>
2018-07-12 00:24:04 +0430

    moved to a ES6 class pure  Component

--- Jcalendar/JMonth/JMonthItem.js
+++ Jcalendar/JMonth/JMonthItem.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { Text, TouchableOpacity, StyleSheet } from "react-native";
-export default props => (
-  <TouchableOpacity
-    onPress={() => {
-      props.setMonth(props.index + 1);
-    }}
-    style={styles.container}
-  >
-    <Text
-      style={[
-        styles.jMonthText,
-        { color: props.currentMonth == props.index + 1 ? "#0029ff" : "black" }
-      ]}
-    >
-      {props.monthName}
-    </Text>
-  </TouchableOpacity>
-);
+export default class JMonthItem extends React.PureComponent {
+  _jMonthPress = () => {
+    this.props.setMonth(this.props.index + 1);
+  };
+  render() {
+    return (
+      <TouchableOpacity onPress={this._jMonthPress} style={styles.container}>
+        <Text
+          style={[
+            styles.jMonthText,
+            {
+              color:
+                this.props.currentMonth == this.props.index + 1
+                  ? "#0029ff"
+                  : "black"
+            }
+          ]}
+        >
+          {this.props.monthName}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     width: 60,
@@ -25,7 +33,7 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   jMonthText: {
-    fontSize: 13,
+    fontSize: 15,
     fontWeight: "bold",
     textAlign: "center",
     textAlignVertical: "center"
