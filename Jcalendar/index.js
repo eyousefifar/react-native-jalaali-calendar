443fb7d2219ab371be7e42aace6b4142891a8f80
erfan yousefifar <eyousefifar@gmail.com>
2018-07-12 00:22:33 +0430

    added props and changed to pureComponents

--- Jcalendar/index.js
+++ Jcalendar/index.js
@@ -1,39 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions, InteractionManager } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import Month from "./Month";
 import WeekDays from "./WeekDays";
 import JMonth from "./JMonth";
 import JYear from "./JYear";
 import { jMonthInfo } from "./utils";
-const { width, height } = Dimensions.get("window");
-export default class Jcalendar extends React.Component {
+export default class Jcalendar extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      selectedYear: 1397,
-      selectedMonth: 2,
-      selectedDay: 13
+      selectedYear: props.selectedDate.year,
+      selectedMonth: props.selectedDate.month,
+      selectedDay: props.selectedDate.month
     };
   }
   _setYear = year => {
     if (this.state.selectedYear != year) {
-      InteractionManager.runAfterInteractions(()=>{
-        this.setState({ selectedYear: year });
-      });
+      this.setState({ selectedYear: year });
     }
   };
   _setMonth = month => {
     if (this.state.selectedMonth != month) {
-      InteractionManager.runAfterInteractions(()=>{
-        this.setState({ selectedMonth: month });
-      }
-      });
+      this.setState({ selectedMonth: month });
+    }
   };
   _setDay = day => {
     if (this.state.selectedDay != day) {
-      InteractionManager.runAfterInteractions(()=> {
-        this.setState({ selectedDay: day });
-      })
+      this.setState({ selectedDay: day });
     }
   };
   render() {
@@ -43,26 +36,26 @@ export default class Jcalendar extends React.Component {
     );
     return (
       <View style={styles.container}>
-        <View style={{ flex: 1 }}>
+        <View style={styles.container}>
           <JYear
-            startYear={1300}
-            endYear={1450}
+            startYear={this.props.startYear}
+            endYear={this.props.endYear}
             setYear={this._setYear}
             currentYear={this.state.selectedYear}
           />
         </View>
-        <View style={{ flex: 1 }}>
+        <View style={styles.container}>
           <JMonth
             currentMonth={this.state.selectedMonth}
             setMonth={this._setMonth}
           />
         </View>
-        <View style={{ flex: 1 }}>
+        <View style={styles.container}>
           <WeekDays />
         </View>
         <View style={{ flex: 7 }}>
           <Month
-            jMonthInformation={jMonthInformation}
+            jMonthInfo={jMonthInformation}
             setDay={this._setDay}
             currentDay={this.state.selectedDay}
           />
