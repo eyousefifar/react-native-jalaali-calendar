6faa7818df2a98a51fc0d01ca3116919538e1d31
erfan yousefifar <eyousefifar@gmail.com>
2018-08-21 16:53:57 +0430

    added TimePicker

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-export { default as Jcalendar} from './Jcalendar';
-export { default as JPicker} from './JPicker';
+export { default as Jcalendar } from "./Jcalendar";
+export { default as JPicker } from "./JPicker";
+export { default as TimePicker } from "./TimePicker";
